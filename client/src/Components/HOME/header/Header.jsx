c25a3c869937bd5c37e26f3d9ce354168817e025
jabulani-creator <charingabiggie@gmail.com>
2022-09-21 01:01:16 +0800

    landing page cleanup

--- client/src/Components/HOME/header/Header.jsx
+++ client/src/Components/HOME/header/Header.jsx
@@ -10,10 +10,8 @@ export const Header = () => {
             Welcome to <br /> Emmasdale Sda <br /> Church
           </h1>
           <p className="HeroPara">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
-            doloribus corporis dolores quibusdam incidunt excepturi consequuntur
-            iusto blanditiis laborum vitae recusandae accusamus, commodi amet
-            dicta veritatis ut architecto molestias autem.
+            The Perfect Place for People Who Are Not, Whatever You Have Been
+            Through, Wherever you have been, This Is The Right Place
           </p>
           <Link to="/new">
             <button className="btnn">I am New to Emmasdale</button>
